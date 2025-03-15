75e862272a48be35f7463f63210a7a1acee1b296
Vitória Oliveira <vitoriaoliveir@gmail.com>
2025-03-15 15:18:46 -0300

    Alterando informações na página _Sobre_

--- app_front-end/app_gerenciamento_users/src/pages/about/page.jsx
+++ app_front-end/app_gerenciamento_users/src/pages/about/page.jsx
@@ -50,16 +50,16 @@ function About() {
                 <div className="container bg-light p-4 rounded shadow">
                     <h2 className="text-primary mb-4">Sobre</h2>
                     <p className="text-secondary">
-                        Este trabalho foi desenvolvido para a disciplina de <strong>Desenvolvimento de Plataformas Web </strong>
+                        Este trabalho foi desenvolvido para a disciplina de <strong>Desenvolvimento de Software em Nuvem </strong>
                         da <strong> Universidade de Fortaleza</strong>, pelos membros da equipe:
                     </p>
                     <ul className="list-unstyled text-secondary">
-                        <li>Antonio Mikael Vasconcelos Aguiar</li>
-                        <li>Sandy Rodrigues do Nascimento</li>
-                        <li>Vitória de Oliveira Almeida</li>
+                        <li>Antonio Mikael Vasconcelos Aguiar - 2326335</li>
+                        <li>Sandy Rodrigues do Nascimento - 2326334</li>
+                        <li>Vitória de Oliveira Almeida - 2325332</li>
                     </ul>
                     <p className="text-secondary">
-                        O objetivo era criar uma aplicação que permitisse aos usuários controlar seu tempo, ajudando-os a organizar e realizar as atividades do dia a dia com mais eficiência e foco. Além disso, esta aplicação possibilita o gerenciamento de usuários autenticados no sistema.
+                    O objetivo era criar uma aplicação que permitisse aos usuários controlar seu tempo, ajudando-os a organizar e realizar as atividades do dia a dia com mais eficiência e foco. Além disso, essa aplicação possibilita o gerenciamento de usuários autenticados no sistema. Levando em consideração que essa aplicação fosse desenvolvida diretamente na nuvem, tanto o front-end quanto o back-end foram implementados em ambiente cloud. Além disso, o código-fonte foi hospedado em um repositório remoto na nuvem.
                     </p>
                 </div>
             </section>
