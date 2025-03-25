ef77d3701b9103ef2070537f7193a8ca7b17d688
sandyrodriguesjs <sandyrodriguesdev@gmail.com>
2025-03-25 13:36:47 -0300

    Alterando cores do texto e card e ajustando a lógica

--- app_front-end/app_gerenciamento_users/src/pages/pomodoro/page.jsx
+++ app_front-end/app_gerenciamento_users/src/pages/pomodoro/page.jsx
@@ -11,9 +11,9 @@ function Pomodoro() {
     const [taskInput, setTaskInput] = useState("");
 
     useEffect(() => {
-        document.body.classList.add('bg-dark', 'text-light', 'text-center');
+        document.body.classList.add('bg-light', 'text-secondary', 'text-center');
         return () => {
-            document.body.classList.remove('bg-dark', 'text-light', 'text-center');
+            document.body.classList.remove('bg-light', 'text-secondary', 'text-center');
         };
     }, []);
 
@@ -61,7 +61,7 @@ function Pomodoro() {
 
     const addTask = () => {
         if (taskInput.trim() !== "") {
-            setTasks([...tasks, { text: taskInput, completed: false }]);
+            setTasks([{ text: taskInput, completed: false, deleted: false }, ...tasks]); // Adiciona a tarefa no topo
             setTaskInput("");
         }
     };
@@ -69,46 +69,55 @@ function Pomodoro() {
     const toggleTaskCompletion = (index) => {
         const updatedTasks = [...tasks];
         updatedTasks[index].completed = !updatedTasks[index].completed;
+
+        // Reorganiza as tarefas para que as não concluídas venham antes das concluídas
         updatedTasks.sort((a, b) => a.completed - b.completed);
+
         setTasks(updatedTasks);
     };
 
     const removeTask = (index) => {
-        setTasks(tasks.filter((_, i) => i !== index));
+        const updatedTasks = [...tasks];
+        updatedTasks.splice(index, 1); // Remove a tarefa de forma definitiva
+        setTasks(updatedTasks);
     };
 
     return (
         <>
             <header className="bg-primary py-3">
                 <div className="container d-flex justify-content-between align-items-center">
-                    <h1 className="h3 mb-0">Controle seu Tempo</h1>
+                <h1 className="h3 mb-0" style={{ color: "#ffffff" }}>Controle seu Tempo</h1>
                     <nav>
                         <Link to="/gerenciamento_users">
-                            <button className="btn btn-link text-light">Gerenciar usuários</button>
+                            <button className="btn btn-link text-light" style={{ textDecoration: "none" }}>Gerenciar usuários</button>
                         </Link>
-                        <button onClick={logout} className="btn btn-link text-light">
+                        <button onClick={logout} className="btn btn-link text-light" style={{ textDecoration: "none" }}>
                             Sair
                         </button>
                     </nav>
                 </div>
             </header>
 
-            <div className="container mt-5">
-                <div className="d-flex justify-content-center mb-3">
-                    <button onClick={() => resetTimer(25 * 60)} className="btn btn-primary mx-1">Pomodoro</button>
-                    <button onClick={() => resetTimer(5 * 60)} className="btn btn-primary mx-1">Pausa Curta</button>
-                    <button onClick={() => resetTimer(15 * 60)} className="btn btn-primary mx-1">Pausa Longa</button>
+            <div className="container mt-5 d-flex justify-content-between">
+                {/* Pomodoro Section */}
+                <div className="pomodoro-section" style={{ flex: 1, textAlign: 'center' }}>
+                    <div className="d-flex justify-content-center mb-3">
+                        <button onClick={() => resetTimer(25 * 60)} className="btn btn-primary mx-1">Pomodoro</button>
+                        <button onClick={() => resetTimer(5 * 60)} className="btn btn-primary mx-1">Pausa Curta</button>
+                        <button onClick={() => resetTimer(15 * 60)} className="btn btn-primary mx-1">Pausa Longa</button>
+                    </div>
+
+                    <div id="display-temporizador" className="display-1 font-weight-bold">
+                        {formatTime(time)}
+                    </div>
+
+                    <button onClick={startTimer} className="btn btn-primary btn-lg mt-3">
+                        {isRunning ? 'PAUSAR' : 'INICIAR'}
+                    </button>
                 </div>
 
-                <div id="display-temporizador" className="display-1 font-weight-bold">
-                    {formatTime(time)}
-                </div>
-
-                <button onClick={startTimer} className="btn btn-primary btn-lg mt-3">
-                    {isRunning ? 'PAUSAR' : 'INICIAR'}
-                </button>
-
-                <div className='mt-5'>
+                {/* To-Do List Section */}
+                <div className="todo-section" style={{ flex: 1, maxWidth: '400px', paddingLeft: '20px' }}>
                     <h3>To-Do List</h3>
                     <div className='input-group mb-3'>
                         <input 
@@ -117,21 +126,47 @@ function Pomodoro() {
                             placeholder='Adicionar tarefa...'
                             value={taskInput}
                             onChange={(e) => setTaskInput(e.target.value)}
+                            style={{ 
+                                height: '40px', 
+                                fontSize: '16px', 
+                                padding: '10px', 
+                                borderRadius: '8px', 
+                                width: 'calc(100% - 40px)' 
+                            }}
                         />
-                        <button onClick={addTask} className='btn btn-primary'>
+                        <button onClick={addTask} className='btn btn-primary' 
+                            style={{
+                                height: '40px', 
+                                padding: '10px', 
+                                borderRadius: '8px', // Deixando as bordas arredondadas
+                                border: '1px solid #007bff', // Cor de borda para combinar com o input
+                                boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+                                display: 'flex', 
+                                alignItems: 'center', 
+                                justifyContent: 'center'
+                            }}
+                        >
                             <FaPlus />
                         </button>
                     </div>
-                    <ul className='list-group'>
+                    <ul className='list-group' style={{ listStyleType: 'none', padding: 0, height: '400px', overflowY: 'auto' }}>
                         {tasks.map((task, index) => (
-                            <li key={index} className={`list-group-item d-flex justify-content-between align-items-center ${task.completed ? 'bg-light text-muted' : ''}`} style={task.completed ? { textDecoration: 'line-through' } : {}}>
+                            <li key={index} 
+                                className={`list-group-item d-flex justify-content-between align-items-center ${task.completed ? 'bg-light text-muted' : ''}`}
+                                style={{
+                                    borderRadius: '10px',
+                                    backgroundColor: task.completed ? '#e9ecef' : '#f1f1f1', // Trocando a cor amarelada por tons mais suaves
+                                    boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+                                    marginBottom: '5px',
+                                    padding: '10px 15px',
+                                    textDecoration: task.completed ? 'line-through' : 'none' // Adiciona a linha cortando o texto
+                                }}>
                                 <div>
                                     <input 
                                         type='checkbox' 
                                         className='form-check-input me-2' 
                                         checked={task.completed}
                                         onChange={() => toggleTaskCompletion(index)}
-                                        disabled={task.completed}
                                     />
                                     {task.text}
                                 </div>
