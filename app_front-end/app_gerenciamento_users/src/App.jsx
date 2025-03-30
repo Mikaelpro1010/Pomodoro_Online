4233821c99f8365c7e5bab5492286e83a40ef54f
sandyrodriguesjs <sandyrodriguesdev@gmail.com>
2025-03-30 20:25:31 -0300

    Alterando o footer

--- app_front-end/app_gerenciamento_users/src/App.jsx
+++ app_front-end/app_gerenciamento_users/src/App.jsx
@@ -117,7 +117,7 @@ function App() {
 
             <section className="mt-5">
                 <footer className="pt-3 pb-3 text-white text-center" style={{ backgroundColor: "#0D47A1" }}>
-                    <p>&copy; 2024. Todos os direitos reservados.</p>
+                    <p>&copy; 2025. Todos os direitos reservados.</p>
                 </footer>
             </section>
 
