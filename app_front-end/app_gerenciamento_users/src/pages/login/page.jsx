f9cf105be8beaad26c6a19b980b379dd094ef4e8
Mikael Aguiar <93383052+Mikaelpro1010@users.noreply.github.com>
2025-06-01 20:27:52 -0300

    Update page.jsx

--- app_front-end/app_gerenciamento_users/src/pages/login/page.jsx
+++ app_front-end/app_gerenciamento_users/src/pages/login/page.jsx
@@ -99,20 +99,20 @@ function Login() {
                     <form className="w-100">
                         <input type="email" name="email" className="form-control mb-3" placeholder="E-mail" ref={inputEmail}
                             style={{ borderRadius: "8px", border: "1px solid #ddd", border: "2px solid", padding: "12px" }}
-                            onFocus={(e) => e.target.style.borderColor = "#1E88E5"}
-                            onBlur={(e) => e.target.style.borderColor = "#ddd"}
+{/*                             onFocus={(e) => e.target.style.borderColor = "#1E88E5"}
+                            onBlur={(e) => e.target.style.borderColor = "#ddd"} */}
                         />
 
                         <input type="password" name="password" className="form-control mb-3" placeholder="Senha" ref={inputPassword}
                             style={{ borderRadius: "8px", border: "1px solid #ddd", border: "2px solid", padding: "12px" }}
-                            onFocus={(e) => e.target.style.borderColor = "#1E88E5"}
-                            onBlur={(e) => e.target.style.borderColor = "#ddd"}
+{/*                             onFocus={(e) => e.target.style.borderColor = "#1E88E5"}
+                            onBlur={(e) => e.target.style.borderColor = "#ddd"} */}
                         />
 
                         <button type="button" onClick={login} className="btn text-white rounded-pill w-100"
                             style={{ backgroundColor: "#1E88E5", padding: "12px", fontSize: "16px", transition: "0.3s" }}
-                            onMouseOver={(e) => e.target.style.backgroundColor = "#0D47A1"}
-                            onMouseOut={(e) => e.target.style.backgroundColor = "#1E88E5"}>
+{/*                             onMouseOver={(e) => e.target.style.backgroundColor = "#0D47A1"}
+                            onMouseOut={(e) => e.target.style.backgroundColor = "#1E88E5"}> */}
                             Entrar
                         </button>
                     </form>
